5fc01d91cedcbadf9f3ac48ea0368789053e5bc0
Bravestone01 <mujtabashah906@gmail.com>
2024-09-30 15:59:10 +0500

    first

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useEffect, useState } from "react";
+import { Button, SectionList, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count,setCount]=useState(0)
+  useEffect(()=>{
+      console.warn("Hello world")
+  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={styles.container}>Life Cycle With UseEffect</Text>
+      <Text style={styles.counting}>{count}</Text>
+
+      <Button title="Update" onPress={()=>setCount(count+1)}/>
+      <View style={styles.resets} /> 
+        <Button title="reset" onPress={()=>setCount(0)} />
+      
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    fontSize: 20,
+    marginTop:40,
+    padding:20,
+    color:"white"
   },
+  counting:{
+    fontSize:40,
+    textAlign:"center",
+    backgroundColor:"black",
+    color:"white",
+    marginTop:200,
+    marginBottom:100,
+    padding:50,
+
+  },
+  resets:{
+    marginTop:50,
+  }
 });
